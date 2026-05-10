e47d73b20f94873724a8ab868a54983e65895eb4
ik-14 <77155825+ik-14@users.noreply.github.com>
2026-05-10 15:02:44 +0100

    Add: guess-number-higher-or-lower - submission-0

---
+++ Data Structures & Algorithms/guess-number-higher-or-lower/submission-0.ts
@@ -0,0 +1,31 @@
+/**
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * function guess(num) {}
+ */
+
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    guessNumber(n: number): number {
+        let low = 1
+        let high = n
+        let mid;
+        while (low <= high) {
+            mid = Math.floor((low + high) / 2)
+            
+            if (guess(mid) === -1) {
+                high = mid - 1
+            } else if (guess(mid) === 1) {
+                low = mid + 1
+            } else {
+                return mid
+            }    
+        }
+    }
+}
